9fd9946941a3ccb75765c0294413485c14164f56
Siddharth Bahuguna <112819453+SiddharthBahuguna@users.noreply.github.com>
2026-09-26 01:07:28 +0530

    Implement passThePillow method in Solution class

---
+++ 2582. Pass the Pillow.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int passThePillow(int n, int time) {
+        int fullRound = time/(n-1);
+        int extraTime = time%(n-1);
+
+        if(fullRound % 2 == 0){
+            return extraTime+1;
+        }
+        else{
+            return n-extraTime;
+        }
+    }
+}
